99c39e77d52f647b8d335da8979a0aa485271eef
cpop <prabhsharan971@gmail.com>
2024-02-10 12:52:18 +0530

    Add : comp for form with styling

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import './App.css';
 import React from 'react';
 import ReactDOM from 'react';
 import Header from './components/Header';
+import Meme from './components/Meme';
+
 
 function App() {
   return (
     <div className="App">
         <Header />
+        <Meme />
     </div>
   );
 }
---
+++ src/components/Meme.js
@@ -0,0 +1,15 @@
+
+
+
+export default function Meme(){
+    return(
+        <div className="main">
+            <form className="form">
+                <input className="form-input" placeholder="Top text" type="text" />
+                <input className="form-input" placeholder="Bottom text" type="text" />
+                <button className="form-button">Get a new meme image</button>
+            </form>
+
+        </div>
+    )
+}
