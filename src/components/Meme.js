e93982b17099c47b74318e1f27aa729f9db8b9bc
cpop <prabhsharan971@gmail.com>
2024-02-10 14:07:02 +0530

    Add : inputl labels

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -3,13 +3,21 @@
 
 export default function Meme(){
     return(
-        <div className="main">
-            <form className="form">
-                <input className="form-input" placeholder="Top text" type="text" />
-                <input className="form-input" placeholder="Bottom text" type="text" />
-                <button className="form-button">Get a new meme image</button>
-            </form>
+        <div className="form">
+                <div className="label">
+                    <label className="form-label">Top text</label>
+                    <input className="form-input" 
+                           placeholder="Shut up" 
+                           type="text" />
+                </div>
+                
+                <div className="label">
+                    <label className="form-label">Bottom text</label>
+                    <input className="form-input" 
+                           placeholder="and take my money" type="text" />
+                </div>
 
+                <button className="form-button">Get a new meme image</button>
         </div>
     )
 }
