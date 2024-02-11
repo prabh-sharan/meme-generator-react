e44510ce7481f2603cb14f1c1cfda9badc150261
cpop <prabhsharan971@gmail.com>
2024-02-11 17:04:11 +0530

    Add: btn eventlistener to gen new img url from meme  and update state

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,7 +1,27 @@
-
-
+import React from "react";
 
 export default function Meme(){
+
+    // current meme
+    const [meme, setMeme] = React.useState({
+        topText :"",
+        bottomText :"",
+        randomImage:""
+    });
+    // for array of all memes
+    const [allMemes, setAllMemes] = React.useState([]); 
+
+    function getMemeImage(){
+        // gen random num
+        const randomNum = Math.floor(Math.random() * allMemes.length);
+        const url = meme[randomNum].url;  
+        //updating state with new url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
+    }
+
     return(
         <div className="form">
                 <div className="label">
@@ -10,14 +30,25 @@ export default function Meme(){
                            placeholder="Shut up" 
                            type="text" />
                 </div>
-                
+
                 <div className="label">
                     <label className="form-label">Bottom text</label>
                     <input className="form-input" 
                            placeholder="and take my money" type="text" />
                 </div>
 
-                <button className="form-button">Get a new meme image</button>
+                <button 
+                    className="form-button"
+                    onClick = {getMemeImage} >
+                    Get a new meme template
+                </button>
+
+                <div className="meme">
+                    <img className="meme-img" src={meme.randomImage}/>
+                    <h2 className="meme-text" >{meme.topText}</h2>
+                    <h2 className="meme-text" > {meme.bottomText}</h2>
+
+                </div>
         </div>
     )
 }
