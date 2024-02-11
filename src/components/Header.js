488522266610a9ca78e9182fc5e23b69099f47c7
cpop <prabhsharan971@gmail.com>
2024-02-11 20:57:59 +0530

    Add github code link with icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 import React from "react";
 import memeface from "../img/meme-face.png";
+import github from "../img/github.svg";
 
 export default function Header(){
     return(
         <header className="header">
             <img className="header-img" src={memeface} alt="meme-face" />
             <h2 className="header-title">Meme Creator</h2>
-            <h3>Download</h3>
+            <a href="https://github.com/prabh-sharan/meme-generator-react">
+                <img className ="header-icon" src = {github} alt="github code" /> 
+            </a>
         </header>
     )
 }
